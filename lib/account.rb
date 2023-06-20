1845745d17eae62e2a901b1559c2ebe5a7e169c1
wimdavies <william.johnston.davies@gmail.com>
2023-06-20 10:42:38 +0100

    Wrote more integration tests

--- lib/account.rb
+++ lib/account.rb
@@ -3,5 +3,7 @@
 class Account
   def initialize; end
 
-  def add(transaction); end
+  def add(transaction)
+    
+  end
 end
--- spec/bank_integration_spec.rb
+++ spec/bank_integration_spec.rb
@@ -24,4 +24,66 @@ RSpec.describe 'Integration' do
     statement = Statement.new(account, io)
     statement.print
   end
+
+  it 'prints a statement of only credits' do
+    io = double :io
+    expect(io).to receive(:puts).with('date || credit || debit || balance').ordered
+    expect(io).to receive(:puts).with('14/01/2023 || 100.00 || || 300.00').ordered
+    expect(io).to receive(:puts).with('13/01/2023 || 100.00 || || 200.00').ordered
+    expect(io).to receive(:puts).with('10/01/2023 || 100.00 || || 100.00').ordered
+
+    transaction1 = Transaction.new(100, '20-06-2023')
+    transaction2 = Transaction.new(100, '21-06-2023')
+    transaction3 = Transaction.new(100, '22-06-2023')
+
+    account = Account.new
+    account.add(transaction1)
+    account.add(transaction2)
+    account.add(transaction3)
+
+    statement = Statement.new(account, io)
+    statement.print
+  end
+
+  it 'prints a statement of only debits, with negative balances' do
+    io = double :io
+    expect(io).to receive(:puts).with('date || credit || debit || balance').ordered
+    expect(io).to receive(:puts).with('12/01/2023 || || 100.00 || -300.00').ordered
+    expect(io).to receive(:puts).with('11/01/2023 || || 100.00 || -200.00').ordered
+    expect(io).to receive(:puts).with('10/01/2023 || || 100.00 || -100.00').ordered
+
+    transaction1 = Transaction.new(-100, '10-01-2023')
+    transaction2 = Transaction.new(-100, '11-01-2023')
+    transaction3 = Transaction.new(-100, '12-01-2023')
+
+    account = Account.new
+    account.add(transaction1)
+    account.add(transaction2)
+    account.add(transaction3)
+
+    statement = Statement.new(account, io)
+    statement.print
+  end
+
+  context 'transactions are added out of date order' do
+    it 'prints transactions sorted by date order' do
+      io = double :io
+      expect(io).to receive(:puts).with('date || credit || debit || balance').ordered
+      expect(io).to receive(:puts).with('14/01/2023 || || 500.00 || 2000.00').ordered
+      expect(io).to receive(:puts).with('13/01/2023 || 2000.00 || || 2500.00').ordered
+      expect(io).to receive(:puts).with('10/01/2023 || 500.00 || || 500.00').ordered
+
+      transaction1 = Transaction.new(2000, '13-01-2023')
+      transaction2 = Transaction.new(-500, '14-01-2023')
+      transaction3 = Transaction.new(500, '10-01-2023')
+
+      account = Account.new
+      account.add(transaction1)
+      account.add(transaction2)
+      account.add(transaction3)
+      
+      statement = Statement.new(account, io)
+      statement.print
+    end
+  end
 end
