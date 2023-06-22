782987cde720e21b796ffb911a180e2a5278fd75
wimdavies <william.johnston.davies@gmail.com>
2023-06-22 15:29:33 +0100

    Began reworking Transaction to initalize with Time.now; rewrote Transaction unit tests

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -4,42 +4,46 @@ require 'transaction'
 
 RSpec.describe Transaction do
   context '#initialize' do
-    it 'with amount as a float, date as a Date object and current balance as a zero float' do
-      expected_date_object = Date.parse('20-06-2023')
-      transaction = Transaction.new(100, '20-06-2023')
+    it 'with amount as a float, date as Time.now and current balance as a zero float' do
+      expected_time = Time.new(2023, 6, 22, 15)
+      allow(Time).to receive(:now).and_return(expected_time)
 
+      transaction = Transaction.new(100)
       expect(transaction.amount).to eq 100.00
-      expect(transaction.date).to eq expected_date_object
+      expect(transaction.date).to eq expected_time
       expect(transaction.balance).to eq 0.0
     end
 
     it 'fails when passed 0 as an amount' do
-      expect { Transaction.new(0, '20-06-2023') }.to raise_error 'Transaction amount cannot be zero'
+      expect { Transaction.new(0) }.to raise_error 'Transaction amount cannot be zero'
     end
   end
 
   context '#amount_string' do
     it 'returns formatted credit string when amount is positive' do
-      transaction = Transaction.new(100, '20-06-2023')
+      transaction = Transaction.new(100)
       expect(transaction.amount_string).to eq ' || 100.00 || || '
     end
 
     it 'returns formatted debit string when amount is negative' do
-      transaction = Transaction.new(-100, '20-06-2023')
+      transaction = Transaction.new(-100)
       expect(transaction.amount_string).to eq ' || || 100.00 || '
     end
   end
 
   context '#date_string' do
     it 'returns formatted date string' do
-      transaction = Transaction.new(100, '20-06-2023')
-      expect(transaction.date_string).to eq '20/06/2023'
+      expected_time = Time.new(2023, 6, 22, 15)
+      allow(Time).to receive(:now).and_return(expected_time)
+
+      transaction = Transaction.new(100)
+      expect(transaction.date_string).to eq '22/06/2023'
     end
   end
 
   context '#balance_string' do
     it 'returns formatted balance string' do
-      transaction = Transaction.new(100, '20-06-2023')
+      transaction = Transaction.new(100)
       expect(transaction.balance_string).to eq '0.00'
     end
   end
