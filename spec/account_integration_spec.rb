15c8cabf18ab1813bd30e3e2d303805085ba8134
wimdavies <william.johnston.davies@gmail.com>
2023-06-22 16:01:24 +0100

    Rewrote account_integration_spec to pass with new transaction design

--- spec/account_integration_spec.rb
+++ spec/account_integration_spec.rb
@@ -8,36 +8,48 @@ RSpec.describe 'Account integration' do
 
   context 'Account#add' do
     it 'adds a single transaction' do
-      transaction = Transaction.new(100, '20-06-2023')
+      transaction = Transaction.new(100)
       account.add(transaction)
-
       expect(account.transactions).to eq [transaction]
     end
 
     it 'adds multiple transactions' do
-      transaction1 = Transaction.new(200, '21-06-2023')
-      transaction2 = Transaction.new(100, '20-06-2023')
+      time1 = Time.new(2023, 6, 22, 12)
+      allow(Time).to receive(:now).and_return(time1)
+      transaction1 = Transaction.new(200)
+
+      time2 = Time.new(2023, 6, 22)
+      allow(Time).to receive(:now).and_return(time2)
+      transaction2 = Transaction.new(100)
+
       account.add(transaction1)
       account.add(transaction2)
-
       expect(account.transactions).to eq [transaction1, transaction2]
     end
 
     it 'sorts the array in place as transactions are added out-of-sequence' do
-      transaction1 = Transaction.new(100, '20-06-2023')
-      transaction2 = Transaction.new(200, '21-06-2023')
-      transaction3 = Transaction.new(200, '22-06-2023')
+      time1 = Time.new(2023, 6, 20)
+      allow(Time).to receive(:now).and_return(time1)
+      transaction1 = Transaction.new(200)
+
+      time2 = Time.new(2023, 6, 21)
+      allow(Time).to receive(:now).and_return(time2)
+      transaction2 = Transaction.new(100)
+
+      time3 = Time.new(2023, 6, 22)
+      allow(Time).to receive(:now).and_return(time3)
+      transaction3 = Transaction.new(100)
+
       account.add(transaction1)
       account.add(transaction2)
       account.add(transaction3)
-
       expect(account.transactions).to eq [transaction3, transaction2, transaction1]
     end
   end
 
   context 'Account#calculate_balances' do
     it 'sets the balance of the first transaction correctly' do
-      transaction = Transaction.new(100, '20-06-2023')
+      transaction = Transaction.new(100)
       account.add(transaction)
       account.calculate_balances
 
@@ -46,13 +58,21 @@ RSpec.describe 'Account integration' do
     end
 
     it 'calculates and sets balances for multiple credit transactions' do
-      transaction1 = Transaction.new(100, '20-06-2023')
-      transaction2 = Transaction.new(100, '21-06-2023')
-      transaction3 = Transaction.new(100, '22-06-2023')
+      time1 = Time.new(2023, 6, 20)
+      allow(Time).to receive(:now).and_return(time1)
+      transaction1 = Transaction.new(100)
+
+      time2 = Time.new(2023, 6, 21)
+      allow(Time).to receive(:now).and_return(time2)
+      transaction2 = Transaction.new(100)
+
+      time3 = Time.new(2023, 6, 22)
+      allow(Time).to receive(:now).and_return(time3)
+      transaction3 = Transaction.new(100)
+
       account.add(transaction1)
       account.add(transaction2)
       account.add(transaction3)
-
       account.calculate_balances
 
       first_balance = account.transactions[-1].balance_string
@@ -64,10 +84,22 @@ RSpec.describe 'Account integration' do
     end
 
     it 'calculates and sets balances for multiple credit and debit transactions' do
-      transaction1 = Transaction.new(200, '20-06-2023')
-      transaction2 = Transaction.new(-100, '21-06-2023')
-      transaction3 = Transaction.new(100, '22-06-2023')
-      transaction4 = Transaction.new(-100, '23-06-2023')
+      time1 = Time.new(2023, 6, 20)
+      allow(Time).to receive(:now).and_return(time1)
+      transaction1 = Transaction.new(200)
+
+      time2 = Time.new(2023, 6, 21)
+      allow(Time).to receive(:now).and_return(time2)
+      transaction2 = Transaction.new(-100)
+
+      time3 = Time.new(2023, 6, 22)
+      allow(Time).to receive(:now).and_return(time3)
+      transaction3 = Transaction.new(100)
+
+      time4 = Time.new(2023, 6, 23)
+      allow(Time).to receive(:now).and_return(time4)
+      transaction4 = Transaction.new(-100)
+
       account.add(transaction1)
       account.add(transaction2)
       account.add(transaction3)
