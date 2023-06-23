8cf07a75f865f5227b4ed238d6bb33b701580202
wimdavies <william.johnston.davies@gmail.com>
2023-06-23 18:23:24 +0100

    RSpec config change

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,8 +29,8 @@ RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
-  config.failure_color = :red
   config.success_color = :yellow
+  config.pending_color = :blue
 
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
